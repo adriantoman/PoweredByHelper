082ada104577cc5b7b565427b946a9dbe15b5a3d
Adrian Toman <adrian.toman@gooddata.com>
2013-10-16 14:28:55 +0200

    Logger problem fix

--- lib/poweredbyhelper.rb
+++ lib/poweredbyhelper.rb
@@ -86,10 +86,10 @@ module PowerByHelper
 
 
     def delete_all_projects(force)
-      $log.info "Deleting project - dry run (to normal run specify force parameter)" if force
+      @@log.info "Deleting project - dry run (to normal run specify force parameter)" if force
       Persistent.project_data.each do |project|
         if (!project.nil? and !project.project_pid.nil?)
-          $log.info "Deleting project #{project.project_pid}"
+          @@log.info "Deleting project #{project.project_pid}"
           project = GoodData::Project[project.project_pid]
           project.delete if force
         end
