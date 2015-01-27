d907fea51ce61a446035b429b5bca1c57948ddc1
adriantoman <adrian.toman@gooddata.com>
2015-01-27 12:26:53 +0100

    version 0.4. 11 -Muf source fix v3

--- lib/muf/muf_collection.rb
+++ lib/muf/muf_collection.rb
@@ -120,16 +120,6 @@ module PowerByHelper
           end
         end
       end
-      # Lets delete the MUF which don't have MUF source file present, but they were set in previous runs
-      Persistent.muf_projects.each do |muf_project|
-        # This mean, that project had some mufs but now it don't have any
-        project = Persistent.project_data.find{|project| project.ident == muf_project.ident}
-        if (!@file_project_mapping.include?(muf_project.ident) and project.status == ProjectData.OK)
-          muf_project.muf_logins.each do |muf_login|
-            muf_login.reset_muf
-          end
-        end
-      end
     end
 
 
@@ -138,6 +128,7 @@ module PowerByHelper
         muf_project.muf_logins.each do |muf_login|
           muf_login.mufs.each do |muf|
             if (!muf.same?)
+
               if (muf.new_values.empty?)
                 muf_login.to_delete
               elsif (muf.values.empty?)
