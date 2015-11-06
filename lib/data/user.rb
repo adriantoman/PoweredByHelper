187e8909547cd8878600002b831f6b0973ca08fb
adriantoman <adrian.toman@gooddata.com>
2015-11-06 16:22:11 +0100

    version 0.4. 15 - Fix of creating empty mufs for users

--- lib/data/user.rb
+++ lib/data/user.rb
@@ -247,12 +247,12 @@ module PowerByHelper
       project_pids.each do |pid|
         if (!@muf_collection.nil?)
           muf_project = @muf_collection.find_muf_project_by_pid(pid)
-        end
-        user_project_data_for_one_pid = Persistent.user_project_data[pid].values
-        user_project_data_for_one_pid.delete_if{|up| users_to_ignore.include?(up.login)}
-        muf_project.delete_logins(users_to_ignore)
+          user_project_data_for_one_pid = Persistent.user_project_data[pid].values
+          user_project_data_for_one_pid.delete_if{|up| users_to_ignore.include?(up.login)}
+          muf_project.delete_logins(users_to_ignore)
 
-        @threading_initial_collection << {"muf_project" => muf_project,"user_project_data" => user_project_data_for_one_pid}
+          @threading_initial_collection << {"muf_project" => muf_project,"user_project_data" => user_project_data_for_one_pid}
+        end
       end
 
       threads = []
